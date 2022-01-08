b80935f85e5ea383ceb0adc059d5e254af52ec12
Jack Malcom <jackxmalcom@gmail.com>
2022-01-08 00:40:57 -0600

    refactor from function

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
         .flatten()
         .flatten()
     {
-        if let Some(battery) = Battery::from(bat_path) {
+        if let Some(battery) = Battery::load(bat_path) {
             println!("{}", battery);
         }
     }
@@ -19,24 +19,31 @@ struct Battery {
 }
 
 impl Battery {
-    fn from(path: DirEntry) -> Option<Self> {
-        if let Ok(entry) = fs::read_dir(path.path()) {
-            let mut out: Option<Self> = None;
-            for e in entry.flatten() {
-                if e.path().file_name().unwrap().to_str().unwrap() == "capacity" {
-                    out = Some(Battery {
-                        name: path.file_name().to_str().unwrap().into(),
-                        capacity: fs::read_to_string(e.path())
-                            .unwrap()
-                            .trim_end_matches('\n')
-                            .into(),
-                    });
-                };
-            }
-            out
-        } else {
-            None
-        }
+    fn load(path: DirEntry) -> Option<Self> {
+        // if let Ok(entry) = fs::read_dir(path.path()) {
+        //     let mut out: Option<Self> = None;
+        //     for e in entry.flatten() {
+        //         if e.path().file_name().unwrap().to_str().unwrap() == "capacity" {
+        //             out = Some(Battery {
+        //                 name: path.file_name().to_str().unwrap().into(),
+        //                 capacity: fs::read_to_string(e.path())
+        //                     .unwrap()
+        //                     .trim_end_matches('\n')
+        //                     .into(),
+        //             });
+        //         };
+        //     }
+        //     out
+        // } else {
+        //     None
+        // }
+        Some(Self {
+            name: path.file_name().to_str()?.into(),
+            capacity: fs::read_to_string(path.path().join("capacity"))
+                .ok()?
+                .trim_end_matches('\n')
+                .into(),
+        })
     }
 }
 
