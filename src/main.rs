78dd1183373f62ac313292e242161147e1ea2f16
Jack Malcom <jackxmalcom@gmail.com>
2022-01-09 00:21:43 -0600

    display manufacturer and model info

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,8 @@ fn main() {
 struct Battery {
     name: String,
     capacity: String,
+    manufacturer: String,
+    model_name: String,
 }
 
 impl Battery {
@@ -34,12 +36,26 @@ impl Battery {
                 .ok()?
                 .trim_end_matches('\n')
                 .into(),
+            manufacturer: fs::read_to_string(path.path().join("manufacturer"))
+                .unwrap_or_default()
+                .trim_end_matches('\n')
+                .into(),
+            model_name: fs::read_to_string(path.path().join("model_name"))
+                .unwrap_or_default()
+                .trim_end_matches('\n')
+                .into(),
         })
     }
 }
 
 impl fmt::Display for Battery {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}: {}%", self.name, self.capacity)
+        write!(
+            f,
+            "{}: {}% - {}",
+            self.name,
+            self.capacity,
+            format!("{} {}", self.manufacturer, self.model_name).trim()
+        )
     }
 }
